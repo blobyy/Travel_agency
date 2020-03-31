02dd30ea6e916241af8f74472b317d26ad4eaac5
Szymon <Szymon@DESKTOP-5QNL757>
2020-03-31 14:04:51 +0200

    Dodaj pliki projektów.

---
+++ Travel_agency/DataaBaseConnect.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Travel_agency
+{
+    class DataBaseConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+
+        public DataBaseConnect()
+        {
+            Initialize();
+        }
+
+
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "";
+            uid = "";
+            password = "";
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
