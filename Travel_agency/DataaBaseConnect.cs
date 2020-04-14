89e152793be176532cdea647144513ed2a2d3d79
Szymon <Szymon@DESKTOP-5QNL757>
2020-04-14 21:20:12 +0200

    Działające połączenie z bazą danych, dodany insert

--- Travel_agency/DataaBaseConnect.cs
+++ Travel_agency/DataaBaseConnect.cs
@@ -25,23 +25,82 @@ namespace Travel_agency
         private void Initialize()
         {
             server = "localhost";
-            database = "";
-            uid = "";
-            password = "";
+            database = "travel_data";
+            uid = "test";
+            password = "1234";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+
         }
 
 
 
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch(MySqlException ex)
+            {
+
+                switch(ex.Number)
+                {
+                    case 0:
+                        System.Windows.Forms.MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        System.Windows.Forms.MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+
+                return false;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        public void Insert(string query )
+        {
+
+            if(this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                cmd.ExecuteNonQuery();
+
+                this.CloseConnection();
+            }
+        }
+
+        
 
 
+        public void Update()
+        {
+            
+        }
 
+        public void Delete()
+        {
 
-
-
-
-
-
-
+        }
 
 
 
--- Travel_agency/Program.cs
+++ Travel_agency/Program.cs
@@ -17,6 +17,19 @@ namespace Travel_agency
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            DataBaseConnect baza = new DataBaseConnect();
+            string komenda = "INSERT INTO `trips` (`Name`, `Destination`, `Date_start`, `Date_end`) VALUES ('testghghghghg', 'testownia', '2020-04-01', '2020-04-16')";
+            baza.Insert(komenda);
+
+
+
+
+
+
+
         }
+
+
     }
 }
